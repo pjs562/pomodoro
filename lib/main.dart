1f163b7ab238d801cb0703146ecd8d964d151672
Joonseo Park <jspark2015@unist.ac.kr>
2023-03-21 16:27:34 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,9 +22,7 @@ class MyApp extends StatelessWidget {
         ),
         cardColor: const Color(0xFFF4EDDB),
       ),
-      home: const Scaffold(
-        body: Center(),
-      )
+      home: const HomeScreen()
     );
   }
 }
